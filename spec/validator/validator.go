1a458f41b5ceabd12ee7847a06d0f13c84219866
utubun <dmytro.strunin@gmail.com>
2024-02-08 07:42:35 +0100

    Initial commit

---
+++ spec/validator/validator.go
@@ -0,0 +1,17 @@
+package gunzip
+
+import (
+	"errors"
+	"io"
+
+	"github.com/gabriel-vasile/mimetype"
+)
+
+func Validate(r io.Reader, t string) (bool, error) {
+	ftype, err := mimetype.DetectReader(r)
+	if err != nil {
+		return false, errors.New("can not detect file type")
+	}
+
+	return ftype.String() == t, nil
+}
