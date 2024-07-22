de5af72b9a4ce518d0ea6a3cd3163bca99ea3834
Dmytro Strunin <dmytro.strunin@gmail.com>
2024-07-22 19:56:13 +0200

    removing unnecessary code

--- quality/qc.go
+++
@@ -1,9 +0,0 @@
-package quality
-
-import "github.com/utubun/ngs/quality/internal/core"
-
-//type QC interface{}
-
-func NewQC() *core.QC {
-	return &core.QC{Bases: make(map[string][]int), Seq: make([]core.Sequence, 1, 1000000)}
-}
